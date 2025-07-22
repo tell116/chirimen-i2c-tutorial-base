38578aa8e62d23587b98693d3ac92d3d9237087a
ha-terai <ha-terai@KTEC-2112PC0103.local>
2025-07-22 11:46:12 +0900

    Once per second

--- app.mjs
+++ app.mjs
@@ -1,13 +1,23 @@
 import {requestI2CAccess, SHT40} from "chirimen";
 
 const i2cAccess = await requestI2CAccess();
+
 // i2cPort生成処理
 const i2cPort = i2cAccess.ports.get(1);
 // SHT40のインスタンス生成処理
 const sht40 = new SHT40(i2cPort, 0x44);
 // SHT40の初期化処理
 await sht40.init();
-// SHT40からデータ（data.humidityと、data.temperature）を読み出す。
-let data = await sht40.readData();
-// 読み出したデータをコンソール出力
-console.dir(data);
+
+let count = 0;
+const interval = setInterval(async function() {
+    // 
+    let data = await sht40.readData();
+    // 
+    console.dir(data);
+    
+    count++;
+    if (count === 5) {
+        clearInterval(interval);
+    }
+}, 1000);
