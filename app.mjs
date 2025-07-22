659969853323d626591dcc33f076d5c55a1ec23c
ha-terai <ha-terai@KTEC-2112PC0103.local>
2025-07-22 11:17:22 +0900

    tutorial

--- app.mjs
+++ app.mjs
@@ -1,3 +1,13 @@
-import {requestI2CAccess} from "chirimen";
+import {requestI2CAccess, SHT40} from "chirimen";
 
 const i2cAccess = await requestI2CAccess();
+// i2cPort生成処理
+const i2cPort = i2cAccess.ports.get(1);
+// SHT40のインスタンス生成処理
+const sht40 = new SHT40(i2cPort, 0x44);
+// SHT40の初期化処理
+await sht40.init();
+// SHT40からデータ（data.humidityと、data.temperature）を読み出す。
+let data = await sht40.readData();
+// 読み出したデータをコンソール出力
+console.dir(data);
