e3c0f09e1784610b6ab374b329f99c5e12f38e3c
tell116 <0116teraharu@gmail.com>
2025-07-23 15:22:25 +0900

    mcp9808作成

--- mcp9808.js
+++ mcp9808.js
@@ -1,23 +1,8 @@
-const MCP9808_I2CADDR_DEFAULT= 0x18;    ///< I2C address
-const MCP9808_REG_CONFIG= 0x01;         ///< MCP9808 config register
-
-const MCP9808_REG_CONFIG_SHUTDOWN= 0x0100   ///< shutdown config
-const MCP9808_REG_CONFIG_CRITLOCKED= 0x0080 ///< critical trip lock
-const MCP9808_REG_CONFIG_WINLOCKED= 0x0040  ///< alarm window lock
-const MCP9808_REG_CONFIG_INTCLR= 0x0020     ///< interrupt clear
-const MCP9808_REG_CONFIG_ALERTSTAT= 0x0010  ///< alert output status
-const MCP9808_REG_CONFIG_ALERTCTRL= 0x0008  ///< alert output control
-const MCP9808_REG_CONFIG_ALERTSEL= 0x0004   ///< alert output select
-const MCP9808_REG_CONFIG_ALERTPOL= 0x0002   ///< alert output polarity
-const MCP9808_REG_CONFIG_ALERTMODE= 0x0001  ///< alert output mode
-
-const MCP9808_REG_UPPER_TEMP= 0x02      ///< upper alert boundary
-const MCP9808_REG_LOWER_TEMP= 0x03      ///< lower alert boundery
-const MCP9808_REG_CRIT_TEMP= 0x04       ///< critical temperature
-const MCP9808_REG_AMBIENT_TEMP= 0x05    ///< ambient temperature
-const MCP9808_REG_MANUF_ID= 0x06        ///< manufacture ID
-const MCP9808_REG_DEVICE_ID= 0x07       ///< device ID
-const MCP9808_REG_RESOLUTION= 0x08      ///< resolution
+const MCP9808_I2CADDR_DEFAULT= 0x18;      ///< I2C address
+const MCP9808_REG_CONFIG= 0x01;           ///< MCP9808 config register
+const MCP9808_REG_CONFIG_SHUTDOWN= 0x0100 ///< shutdown config
+const MCP9808_REG_AMBIENT_TEMP= 0x05      ///< ambient temperature
+const MCP9808_REG_RESOLUTION= 0x08        ///< resolution
 
 class MCP9808{
     constructor(i2cPort,slaveAddress){
@@ -94,14 +79,14 @@ class MCP9808{
   async read16(reg){
     let value = await this.i2cSlave.read16(reg);
     // エンディアン変換
-    const low = value & 0xFF;
-    const high = (value >> 8) & 0xFF;
+    let low = value & 0xFF;
+    let high = (value >> 8) & 0xFF;
     return (low << 8) | high;
   }
   async write16(reg,value){
     // エンディアン変換
-    const low = value & 0xFF;
-    const high = (value >> 8) & 0xFF;
+    let low = value & 0xFF;
+    let high = (value >> 8) & 0xFF;
     await this.i2cSlave.write16(reg,((low << 8) | high));
   }
 }
