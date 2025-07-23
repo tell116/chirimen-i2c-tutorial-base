f1318e58000f67f8823aeb2f238993dd84ca471d
ha-terai <ha-terai@KTEC-2112PC0103.local>
2025-07-23 12:01:12 +0900

    mcp9808作成

--- app-mcp9808.mjs
+++ app-mcp9808.mjs
@@ -14,26 +14,26 @@ await mcp9808.init();
 const interval = setInterval(async function() {
   
   mcp9808.setResolution(0);
-  let data_0t = mcp9808.readTempC;
-  let data_0f = mcp9808.readTempF;
+  let data_0t = mcp9808.readTempC();
+  let data_0f = mcp9808.readTempF();
   console.dir("mode=0");
   console.dir({"T":data_0t,"F":data_0f});
   
   mcp9808.setResolution(1);
-  let data_1t = mcp9808.readTempC;
-  let data_1f = mcp9808.readTempF;
+  let data_1t = mcp9808.readTempC();
+  let data_1f = mcp9808.readTempF();
   console.dir("mode=1");
   console.dir({"T":data_1t,"F":data_1f});
 
   mcp9808.setResolution(2);
-  let data_2t = mcp9808.readTempC;
-  let data_2f = mcp9808.readTempF;
+  let data_2t = mcp9808.readTempC();
+  let data_2f = mcp9808.readTempF();
   console.dir("mode=2");
   console.dir({"T":data_2t,"F":data_2f});
 
   mcp9808.setResolution(3);
-  let data_3t = mcp9808.readTempC;
-  let data_3f = mcp9808.readTempF;
+  let data_3t = mcp9808.readTempC();
+  let data_3f = mcp9808.readTempF();
   console.dir("mode=3");
   console.dir({"T":data_3t,"F":data_3f});
 
