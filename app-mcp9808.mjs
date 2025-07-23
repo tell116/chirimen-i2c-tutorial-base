fa24d370b897a98b9f8b72a1bdf1abf8c4bf26e6
tell116 <0116teraharu@gmail.com>
2025-07-23 15:30:41 +0900

    mcp9808作成

--- app-mcp9808.mjs
+++ app-mcp9808.mjs
@@ -12,49 +12,14 @@ const mcp9808 = new MCP9808(i2cPort, 0x18);
 await mcp9808.init();
 
 await mcp9808.wake();
+await mcp9808.setResolution(3);
 
 const interval = setInterval(async function() { 
 
-  for(let i=0;i<4;i++){
-    await mcp9808.setResolution(i);
-    let mode = await mcp9808.getResolution();
-    let data_t = await mcp9808.readTempC();
-    let data_f = await mcp9808.readTempF();
-    console.dir(mode);
-    console.dir({"T":data_t,"F":data_f});
-  }
-
-  /*
-  await mcp9808.setResolution(0);
   let mode = await mcp9808.getResolution();
-  let data_0t = await mcp9808.readTempC();
-  let data_0f = await mcp9808.readTempF();
+  let data_t = await mcp9808.readTempC();
+  let data_f = await mcp9808.readTempF();
   console.dir(mode);
-  console.dir({"T":data_0t,"F":data_0f});
-  
-  await mcp9808.setResolution(1);
-  mode = await mcp9808.getResolution();
-  let data_1t = await mcp9808.readTempC();
-  let data_1f = await mcp9808.readTempF();
-  console.dir(mode);
-  console.dir({"T":data_1t,"F":data_1f});
-
-  await mcp9808.setResolution(2);
-  mode = await mcp9808.getResolution();
-  let data_2t = await mcp9808.readTempC();
-  let data_2f = await mcp9808.readTempF();
-  console.dir(mode);
-  console.dir({"T":data_2t,"F":data_2f});
-
-  await mcp9808.setResolution(3);
-  mode = await mcp9808.getResolution();
-  let data_3t = await mcp9808.readTempC();
-  let data_3f = await mcp9808.readTempF();
-  console.dir(mode);
-  console.dir({"T":data_3t,"F":data_3f});
-  */
-
+  console.dir({"T":data_t,"F":data_f});
 
 }, 500);
-
-//await mcp9808.shutdown();
