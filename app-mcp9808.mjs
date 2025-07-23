a62639bf2e823b68683e50cc51f0bdaad1173eec
ha-terai <ha-terai@KTEC-2112PC0103.local>
2025-07-23 12:29:16 +0900

    mcp9808作成

--- app-mcp9808.mjs
+++ app-mcp9808.mjs
@@ -11,37 +11,35 @@ const mcp9808 = new MCP9808(i2cPort, 0x18);
 // 初期化処理
 await mcp9808.init();
 
+await mcp9808.wake();
 
+const interval = setInterval(async function() { 
 
-const interval = setInterval(async function() {
-  
-  mcp9808.wake();
-
-  mcp9808.setResolution(0);
-  let mode = mcp9808.getResolution();
-  let data_0t = mcp9808.readTempC();
-  let data_0f = mcp9808.readTempF();
+  await mcp9808.setResolution(0);
+  let mode = await mcp9808.getResolution();
+  let data_0t = await mcp9808.readTempC();
+  let data_0f = await mcp9808.readTempF();
   console.dir(mode);
   console.dir({"T":data_0t,"F":data_0f});
   
-  mcp9808.setResolution(1);
-  let data_1t = mcp9808.readTempC();
-  let data_1f = mcp9808.readTempF();
+  await mcp9808.setResolution(1);
+  let data_1t = await mcp9808.readTempC();
+  let data_1f = await mcp9808.readTempF();
   console.dir("mode=1");
   console.dir({"T":data_1t,"F":data_1f});
 
-  mcp9808.setResolution(2);
-  let data_2t = mcp9808.readTempC();
-  let data_2f = mcp9808.readTempF();
+  await mcp9808.setResolution(2);
+  let data_2t = await mcp9808.readTempC();
+  let data_2f = await mcp9808.readTempF();
   console.dir("mode=2");
   console.dir({"T":data_2t,"F":data_2f});
 
-  mcp9808.setResolution(3);
-  let data_3t = mcp9808.readTempC();
-  let data_3f = mcp9808.readTempF();
+  await mcp9808.setResolution(3);
+  let data_3t = await mcp9808.readTempC();
+  let data_3f = await mcp9808.readTempF();
   console.dir("mode=3");
   console.dir({"T":data_3t,"F":data_3f});
 
 }, 500);
 
-mcp9808.shutdown();
+await mcp9808.shutdown();
