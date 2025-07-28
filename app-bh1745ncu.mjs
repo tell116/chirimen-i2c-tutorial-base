0ba85ae527c76caae4cf1f6c4ed3aaca30f3a1fa
tell116 <0116teraharu@gmail.com>
2025-07-28 14:06:27 +0900

    revise js&mjs

--- app-bh1745ncu.mjs
+++ app-bh1745ncu.mjs
@@ -1,4 +1,4 @@
-import {requestI2CAccess, SHT40} from "chirimen";
+import {requestI2CAccess} from "chirimen";
 // import BH1745NCU from "@chirimen/bh1745ncu";
 import BH1745NCU from "./bh1745ncu.js";
 // カラーセンサー
@@ -12,15 +12,10 @@ const bh1745ncu = new BH1745NCU(i2cPort, 0x39);
 // 初期化処理
 await bh1745ncu.init();
 
-let count = 0;
 const interval = setInterval(async function() {
     
     let data = await bh1745ncu.get_val();
     
     console.dir({"RED":data[0], "GREEN":data[1], "BLUE":data[2], "CLEAR":data[3]});
 
-    count++;
-    if (count === 5) {
-        clearInterval(interval);
-    }
 }, 1000);
--- bh1745ncu.js
+++ bh1745ncu.js
@@ -6,15 +6,11 @@
 const BH1745NUC_DEVICE_ADDRESS_38 = 0x38;// 7bit Addrss
 const BH1745NUC_DEVICE_ADDRESS_39 = 0x39;// 7bit Addrss
 
-const BH1745NUC_RED_DATA_LSB = 0x50;
-
-const BH1745NUC_PART_ID_VAL = 0x0B;            
-const BH1745NUC_MANUFACT_ID_VAL = 0xE0;         
-
 const BH1745NUC_SYSTEM_CONTROL = 0x40;              
 const BH1745NUC_MODE_CONTROL1 = 0x41;                
 const BH1745NUC_MODE_CONTROL2 = 0x42;              
-const BH1745NUC_MODE_CONTROL3 = 0x44;            
+const BH1745NUC_MODE_CONTROL3 = 0x44;   
+const BH1745NUC_RED_DATA_LSB = 0x50;         
 const BH1745NUC_MANUFACTURER_ID = 0x92;           
 
 const BH1745NUC_MODE_CONTROL1_MEAS_TIME160MS = 0x00; 
@@ -25,7 +21,7 @@ const BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16 = 2;
 const BH1745NUC_MODE_CONTROL2_RGBC_EN = (1 << 4);       
 
 const BH1745NUC_MODE_CONTROL1_VAL = BH1745NUC_MODE_CONTROL1_MEAS_TIME160MS;
-const BH1745NUC_MODE_CONTROL2_VAL = BH1745NUC_MODE_CONTROL2_RGBC_EN | BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16;
+const BH1745NUC_MODE_CONTROL2_VAL = (BH1745NUC_MODE_CONTROL2_RGBC_EN | BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16);
 const BH1745NUC_MODE_CONTROL3_VAL = 0x02; 
 
 class BH1745NUC{
@@ -43,7 +39,7 @@ class BH1745NUC{
     this.write(BH1745NUC_MODE_CONTROL3, [BH1745NUC_MODE_CONTROL3_VAL]);
   }
 
-  async get_rawval(){///
+  async get_rawval(){
     const data = await this.read(BH1745NUC_RED_DATA_LSB, 8);
     console.dir({"r0":data[0], "r1":data[1], "r2":data[2], "r3":data[3],"r4":data[4], "r5":data[5], "r6":data[6], "r7":data[7]});///
     return (data);
@@ -51,7 +47,7 @@ class BH1745NUC{
 
   async get_val(){
     let val = await this.get_rawval();
-    let data = Array(8);
+    let data = Array(4);
 
     data[0] =  (val[1] << 8) | val[0];
     data[1] =  (val[3] << 8) | val[2];
