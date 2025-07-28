f80f8222bccadb661787a9275383c3a3a98ef9b2
tell116 <0116teraharu@gmail.com>
2025-07-28 19:28:30 +0900

    add eslint

--- app-bh1745ncu.mjs
+++ app-bh1745ncu.mjs
@@ -1,4 +1,4 @@
-import {requestI2CAccess} from "chirimen";
+import { requestI2CAccess } from "chirimen";
 // import BH1745NCU from "@chirimen/bh1745ncu";
 import BH1745NCU from "./bh1745ncu.js";
 // カラーセンサー
@@ -12,10 +12,8 @@ const bh1745ncu = new BH1745NCU(i2cPort, 0x39);
 // 初期化処理
 await bh1745ncu.init();
 
-const interval = setInterval(async function() {
-    
-    let data = await bh1745ncu.get_val();
-    
-    console.dir({"RED":data[0], "GREEN":data[1], "BLUE":data[2], "CLEAR":data[3]});
+const interval = setInterval(async function () {
+  let data = await bh1745ncu.get_val();
 
+  console.dir({ RED: data[0], GREEN: data[1], BLUE: data[2], CLEAR: data[3] });
 }, 1000);
--- bh1745ncu.js
+++ bh1745ncu.js
@@ -2,30 +2,30 @@
 /// https://github.com/SWITCHSCIENCE/samplecodes/blob/master/Conta/2773_BH1745NUC_module/BH1745NUC.zip
 /// https://github.com/SWITCHSCIENCE/samplecodes/blob/master/Conta/2773_BH1745NUC_module/BH1745NUC_module/BH1745NUC_module.ino
 
+const BH1745NUC_DEVICE_ADDRESS_38 = 0x38; // 7bit Addrss
+const BH1745NUC_DEVICE_ADDRESS_39 = 0x39; // 7bit Addrss
 
-const BH1745NUC_DEVICE_ADDRESS_38 = 0x38;// 7bit Addrss
-const BH1745NUC_DEVICE_ADDRESS_39 = 0x39;// 7bit Addrss
+const BH1745NUC_SYSTEM_CONTROL = 0x40;
+const BH1745NUC_MODE_CONTROL1 = 0x41;
+const BH1745NUC_MODE_CONTROL2 = 0x42;
+const BH1745NUC_MODE_CONTROL3 = 0x44;
+const BH1745NUC_RED_DATA_LSB = 0x50;
+const BH1745NUC_MANUFACTURER_ID = 0x92;
 
-const BH1745NUC_SYSTEM_CONTROL = 0x40;              
-const BH1745NUC_MODE_CONTROL1 = 0x41;                
-const BH1745NUC_MODE_CONTROL2 = 0x42;              
-const BH1745NUC_MODE_CONTROL3 = 0x44;   
-const BH1745NUC_RED_DATA_LSB = 0x50;         
-const BH1745NUC_MANUFACTURER_ID = 0x92;           
+const BH1745NUC_MODE_CONTROL1_MEAS_TIME160MS = 0x00;
 
-const BH1745NUC_MODE_CONTROL1_MEAS_TIME160MS = 0x00; 
-
-const BH1745NUC_MODE_CONTROL2_ADC_GAIN_X1 = 0; 
+const BH1745NUC_MODE_CONTROL2_ADC_GAIN_X1 = 0;
 const BH1745NUC_MODE_CONTROL2_ADC_GAIN_X2 = 1;
 const BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16 = 2;
-const BH1745NUC_MODE_CONTROL2_RGBC_EN = (1 << 4);       
+const BH1745NUC_MODE_CONTROL2_RGBC_EN = 1 << 4;
 
 const BH1745NUC_MODE_CONTROL1_VAL = BH1745NUC_MODE_CONTROL1_MEAS_TIME160MS;
-const BH1745NUC_MODE_CONTROL2_VAL = (BH1745NUC_MODE_CONTROL2_RGBC_EN | BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16);
-const BH1745NUC_MODE_CONTROL3_VAL = 0x02; 
+const BH1745NUC_MODE_CONTROL2_VAL =
+  BH1745NUC_MODE_CONTROL2_RGBC_EN | BH1745NUC_MODE_CONTROL2_ADC_GAIN_X16;
+const BH1745NUC_MODE_CONTROL3_VAL = 0x02;
 
-class BH1745NUC{
-    constructor(i2cPort,slaveAddress){
+class BH1745NUC {
+  constructor(i2cPort, slaveAddress) {
     this.i2cPort = i2cPort;
     this.i2cSlave = null;
     this.slaveAddress = slaveAddress;
@@ -39,30 +39,39 @@ class BH1745NUC{
     await this.write(BH1745NUC_MODE_CONTROL3, [BH1745NUC_MODE_CONTROL3_VAL]);
   }
 
-  async get_rawval(){
+  async get_rawval() {
     const data = await this.read(BH1745NUC_RED_DATA_LSB, 8);
-    console.dir({"r0":data[0], "r1":data[1], "r2":data[2], "r3":data[3],"r4":data[4], "r5":data[5], "r6":data[6], "r7":data[7]});///
-    return (data);
+    console.dir({
+      r0: data[0],
+      r1: data[1],
+      r2: data[2],
+      r3: data[3],
+      r4: data[4],
+      r5: data[5],
+      r6: data[6],
+      r7: data[7],
+    }); ///
+    return data;
   }
 
-  async get_val(){
+  async get_val() {
     let val = await this.get_rawval();
     let data = Array(4);
 
-    data[0] =  (val[1] << 8) | val[0];
-    data[1] =  (val[3] << 8) | val[2];
-    data[2] =  (val[5] << 8) | val[4];
-    data[3] =  (val[7] << 8) | val[6];
-    console.dir({"d0":data[0], "d1":data[1], "d2":data[2], "d3":data[3]});///
-    return (data);
+    data[0] = (val[1] << 8) | val[0];
+    data[1] = (val[3] << 8) | val[2];
+    data[2] = (val[5] << 8) | val[4];
+    data[3] = (val[7] << 8) | val[6];
+    console.dir({ d0: data[0], d1: data[1], d2: data[2], d3: data[3] }); ///
+    return data;
   }
 
-  async write(memory_address, data){
+  async write(memory_address, data) {
     await this.i2cSlave.writeByte(memory_address);
     await this.i2cSlave.writeBytes(data);
   }
 
-  async read(memory_address, size){
+  async read(memory_address, size) {
     await this.i2cSlave.writeByte(memory_address);
     return await this.i2cSlave.readBytes(size);
   }
